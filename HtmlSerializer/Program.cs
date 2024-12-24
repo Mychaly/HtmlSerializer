1865ff59e1f2865024ca249e5187c429bb867a2f
Mychaly <mi3238086@gmail.com>
2024-12-24 14:55:21 +0200

    first commit

---
+++ HtmlSerializer/Program.cs
@@ -0,0 +1,20 @@
+﻿using HtmlSerializer;
+using System;
+using System.Threading.Tasks;
+
+namespace HtmlProcessor
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HtmlSerialize loader = new HtmlSerialize();
+            string url = "https://www.example.com";
+
+            string htmlContent = await loader.Load(url);
+
+            Console.WriteLine(htmlContent);
+        }
+
+    }
+}
